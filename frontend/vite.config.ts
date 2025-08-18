f6f66452bb3c9933d5e2aa09e2dd7014d22aebde
VarniG <vgarg@codal.com>
2025-08-18 14:51:50 +0530

    Serve react build bundle via nginx

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,9 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // same as 0.0.0.0
+    host: "0.0.0.0",
     port: 5173,
-    strictPort: true,
-    hmr: { clientPort: 5173 },
   },
 });
